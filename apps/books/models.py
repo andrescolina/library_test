7f9bf734c8e8c34691eac642670f2fae99b70d91
Andres Colina <andrescolmar@gmail.com>
2021-06-21 22:22:47 -0500

    Model db

---
+++ apps/books/models.py
@@ -0,0 +1,50 @@
+
+from core.db import metadata
+from apps.auth.models import users
+import sqlalchemy
+
+
+book = sqlalchemy.Table(
+    "book",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("title", sqlalchemy.String(16), nullable=False),
+    sqlalchemy.Column("subtitle", sqlalchemy.String(16), nullable=False),
+    sqlalchemy.Column("date_publish", sqlalchemy.Date(), nullable=False),
+    sqlalchemy.Column("editor", sqlalchemy.String(16), nullable=False),
+    sqlalchemy.Column("description", sqlalchemy.String(200), nullable=False),
+    sqlalchemy.Column("image", sqlalchemy.String(100), nullable=False),
+    sqlalchemy.Column("user_created", sqlalchemy.ForeignKey(users.c.id))
+)
+
+category = sqlalchemy.Table(
+    "categories",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("name", sqlalchemy.String(16), nullable=False),
+    sqlalchemy.Column("description", sqlalchemy.String(16), nullable=True)
+)
+
+authors = sqlalchemy.Table(
+    "authors",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("name", sqlalchemy.String(16), nullable=False),
+    sqlalchemy.Column("description", sqlalchemy.String(16), nullable=True)
+)
+
+category_book = sqlalchemy.Table(
+    "categories_book",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("book_id", sqlalchemy.ForeignKey(users.c.id)),
+    sqlalchemy.Column("category_id", sqlalchemy.ForeignKey(category.c.id))
+)
+
+authors_book = sqlalchemy.Table(
+    "authors_book",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("book_id", sqlalchemy.ForeignKey(users.c.id)),
+    sqlalchemy.Column("author_id", sqlalchemy.ForeignKey(authors.c.id))
+)
